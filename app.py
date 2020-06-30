052d693e0b841edaca6e06fb8aa374d390fadfec
8bitjustin <j.olson.digital@gmail.com>
2020-06-29 18:25:00 -0700

    Lotta updates.

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+import time
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/success", methods=['POST'])
+def success():
+    return render_template("success.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
