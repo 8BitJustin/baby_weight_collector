92fdb04fb4e967fa82a2c88acef797cf704416d8
8bitjustin <j.olson.digital@gmail.com>
2020-06-30 14:47:15 -0700

    Changed guesser_name and guesser_guess to name and guess, respectively, within index.html. Also added if statement to success function.

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ def index():
 
 @app.route("/success", methods=['POST'])
 def success():
-    return render_template("success.html")
+    if request.method == 'POST':
+        guesser = request.form['name']
+        guess = request.form['guess']
+        print(guesser, guess)
+        return render_template("success.html")
 
 
 if __name__ == "__main__":
