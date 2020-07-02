043272076a57b75bf70a988ae48ff7f54040732a
8bitjustin <j.olson.digital@gmail.com>
2020-07-01 17:05:25 -0700

    Comments added to app.py.

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ db = SQLAlchemy(app)
 
 
 class Data(db.Model):
+    """
+    Data model which creates table within database
+    """
     __tablename__ = "data"
     id = db.Column(db.Integer, primary_key = True)
     name_ = db.Column(db.String(20), unique = True)
@@ -20,18 +23,31 @@ class Data(db.Model):
 
 
 def avg():
+    """
+    Accesses database using query, finds the average between current guesses
+    :return: The average of guesses
+    """
     average = db.session.query(func.avg(Data.guess_)).scalar()
     average = round(average, 1)
     return average
 
 
 def count():
+    """
+    Accesses the number of total 'id' within database
+    :return: Total amount of guesses
+    """
     user_count = db.session.query(func.count(Data.id)).scalar()
     return user_count
 
 
 @app.route("/")
 def index():
+    """
+    Index page. Utilizes both count and avg functions
+    :return: The index.html file, and both the average and count to display
+    within the index.html file
+    """
     count()
     avg()
     return render_template("index.html", average=avg(), count=count())
@@ -39,6 +55,13 @@ def index():
 
 @app.route("/success", methods=['POST'])
 def success():
+    """
+    Success page. If method is POST (which it will be when utilized),
+    name and guess variables are created. If they do not match what is in
+    the database, they are added to the database.
+    :return: Either the success.html (if name/guess aren't already in the
+    db, or the index.html will refresh with an error message
+    """
     if request.method == 'POST':
         name = request.form['name']
         guess = request.form['guess']
